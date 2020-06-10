76950cef9b1a4c348d7fd3ac2369dbe4748ca58c
sbuDiction <nsbu68501@gmail.com>
2020-06-10 15:40:12 +0200

    second commmit

--- git_hub.sh
+++ git_hub.sh
@@ -1,19 +1,1 @@
 #!/bin/bash
-
-
-function get_user_pass() {
-read -p "Enter a password " pass
-    
-    if test "$pass" = "codex123"
-    then
-         echo "Password verified."
-        
-    else
-         echo "Access denied."	
-    fi
-
-
-
-}
-
-get_user_pass
